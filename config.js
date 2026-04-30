98c8e4e6fb9c48b3ecffd85ee2b4b0b8b3aca712
謝孟寰 <k86310451@gmail.com>
2026-04-30 11:21:41 +0800

    Key連線

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: 'https://YOUR_PROJECT_REF.supabase.co',
-  anonKey: 'YOUR_SUPABASE_ANON_KEY'
+  url: 'https://sjnamtygmtlfbweaymdg.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqbmFtdHlnbXRsZmJ3ZWF5bWRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1MTMwNDcsImV4cCI6MjA5MzA4OTA0N30.zxp-tnoO9LUDOblIvvDU0UFV2e_ieDkarwTX8fqjSFo'
 }
